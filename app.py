03830a25d95c55a1ca63afc59a80b6132387633d
THARANISH J <157452382+Tharanish-13@users.noreply.github.com>
2024-11-08 17:16:18 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 class Stack:
     def __init__(self):
         self.items = []
-
+        
     def is_empty(self):
         return len(self.items) == 0
 
